baf225648c3f6dd0f067fc150cd7b484c4cf7cad
nissimamira <nisimamira@gmail.com>
2025-12-03 20:36:23 +0200

    Initial iOS project setup

---
+++ EasitOpen/EasitOpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasitOpenApp.swift
+//  EasitOpen
+//
+//  Created by nissim amira on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EasitOpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
